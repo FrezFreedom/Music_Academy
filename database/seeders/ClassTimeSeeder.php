6f8050b9d1c11ec82e53ca7cb57791fe6226dfd8
frezfreedom <frez1374@gmail.com>
2022-07-25 14:51:21 +0430

    Add models files and seeders

---
+++ database/seeders/ClassTimeSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ClassTimeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
